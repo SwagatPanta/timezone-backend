08e2736a53783569236c1b255578bd339c5cf862
Swagat Panta <you@example.com>
2025-02-03 16:17:40 +0545

    Initial commit : spring boot backed setup

---
+++ timezone-backend/src/main/java/com/swagat/timezone_backend/TimezoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swagat.timezone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimezoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimezoneBackendApplication.class, args);
+	}
+
+}
---
+++ timezone-backend/src/test/java/com/swagat/timezone_backend/TimezoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swagat.timezone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimezoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
